5fd5ba8eeb0e4aa1fe67d0ff08c44b938f2be28f
Kevin Boosten <kboosten@ilionx.com>
2021-04-08 16:03:44 +0200

    ci: update app center scripts

--- ios/App/appcenter-post-clone.sh
+++ ios/App/appcenter-post-clone.sh
@@ -23,6 +23,9 @@ set -x
 # go to root of project
 cd ../..
 
+echo 'NodeJS Version: '
+node -v
+
 
 # The BUILD_ENV will be set with the expected environment (dev, tst, acc, prd) based on the scheme (iOS) or buildType (Android)
 
@@ -49,7 +52,7 @@ else
     # Each configured configuration in your angular.json can use fileReplacements to substibute environment specific variables
     # We use 'production' as our main configuration. Any appended configuration will override previously set configuration.
     # This way we can inherit config from production but override fileReplacements for example
-    npm run build -- --configuration=production,$BUILD_ENV
+    npm run build -- --configuration=production
 fi
 
 # copy the web assets to the native projects and updates the native plugins and dependencies based in package.json
